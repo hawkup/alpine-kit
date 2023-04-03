b1c2885c07c590ed44fd0474b1763b2b701bbca2
hawkup <spiritsquare@gmail.com>
2023-04-03 18:14:54 +0700

    Add button story

--- src/button/polaris/button.stories.ts
+++ src/button/polaris/button.stories.ts
@@ -1,7 +1,50 @@
 import button from './button.html?raw'
+import type { Meta, StoryObj } from '@storybook/html'
 
-export default {
-  title: 'Components/Button/Polaris - Tailwind',
+type ButtonArgs = {
+  intent: 'basic' | 'primary' | 'destructive'
+  size: 'medium' | 'large' | 'slim'
+  text: string
 }
 
-export const Basic = () => button
+const meta: Meta<ButtonArgs> = {
+  title: 'Components/Button/Polaris - Tailwind',
+  component: button,
+  argTypes: {
+    intent: {
+      options: ['basic', 'primary', 'destructive'],
+      control: { type: 'select' }
+    },
+    size: {
+      options: ['medium', 'large', 'slim'],
+      control: { type: 'select' }
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<ButtonArgs>
+
+export const Basic: Story = {
+  render: (args) => {
+    const script = `
+      <script type="text/javascript">
+        window.buttonArgs = {};
+        window.buttonArgs.intent = '${args.intent}';
+        window.buttonArgs.size = '${args.size}';
+        window.buttonArgs.text = '${args.text}';
+        window.buttonArgs.onClick = () => {
+          alert(window.buttonArgs.text);
+        };
+      </script>
+    `
+
+    return `${script}${button}`
+  },
+  args: {
+    intent: 'basic',
+    size: 'medium',
+    text: 'Hello World'
+  }
+}
